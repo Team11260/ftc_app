30b961356c5e4122cc82baa64111ddd2a4b52c5f
MatthewOates36 <uni.oates@gmail.com>
2019-05-27 16:23:02 -0600

    Updated robot class again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examplebase/hardware/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examplebase/hardware/Robot.java
@@ -1,15 +1,7 @@
 package org.firstinspires.ftc.teamcode.examplebase.hardware;
 
 import org.firstinspires.ftc.teamcode.framework.userhardware.inputs.sensors.ExpansionHubMonitor;
-import org.firstinspires.ftc.teamcode.framework.userhardware.inputs.sensors.vision.SamplePosition;
-import org.firstinspires.ftc.teamcode.framework.userhardware.inputs.sensors.vision.tensorflow.TensorFlowImpl;
-import org.firstinspires.ftc.teamcode.framework.userhardware.paths.Path;
 import org.firstinspires.ftc.teamcode.framework.util.AbstractRobot;
-import org.firstinspires.ftc.teamcode.framework.util.RobotCallable;
-
-import static org.firstinspires.ftc.teamcode.framework.userhardware.DoubleTelemetry.LogMode.INFO;
-import static org.firstinspires.ftc.teamcode.framework.userhardware.DoubleTelemetry.LogMode.TRACE;
-import static org.firstinspires.ftc.teamcode.framework.userhardware.inputs.sensors.vision.SamplePosition.UNKNOWN;
 
 public class Robot extends AbstractRobot {
 
@@ -43,7 +35,6 @@ public class Robot extends AbstractRobot {
     }
 
     public void stop() {
-        if (RobotState.currentMatchState == RobotState.MatchState.AUTONOMOUS) stopTensorFlow();
         hardware.stop();
     }
 }
