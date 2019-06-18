5313052d6ea1aa659b4edc0dfbbe019336ad846b
MatthewOates36 <af4family@gmail.com>
2019-06-17 21:06:09 -0600

    Rishi and Milan

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rishiandmilan/RishiMilanOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rishiandmilan/RishiMilanOpMode.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.rishiandmilan;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 import org.firstinspires.ftc.teamcode.framework.abstractopmodes.AbstractAuton;
@@ -11,7 +13,8 @@ import org.upacreekrobotics.dashboard.Config;
 
 @Config
 public class RishiMilanOpMode extends AbstractAuton {
-
+        private DcMotor dcMotorLeft;
+        private DcMotor dcMotorRight;
 
 
     @Override
@@ -21,14 +24,20 @@ public class RishiMilanOpMode extends AbstractAuton {
 
     @Override
     public void Init() {
-
+        dcMotorLeft=hardwareMap.dcMotor.get("left");
+        dcMotorRight=hardwareMap.dcMotor.get("right");
+        dcMotorRight.setDirection(DcMotor.Direction.REVERSE);
 
 
     }
 
     @Override
     public void Run() {
-
+        dcMotorLeft.setPower(1);
+        dcMotorRight.setPower(1);
+        delay(2000);
+        dcMotorLeft.setPower(0);
+        dcMotorRight.setPower(0);
 
 
     }
